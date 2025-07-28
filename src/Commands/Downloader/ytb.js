ae23a0fb2a74209944b97c55a1778dea76e30f0b
goatbot2025 <musiclovers1234.com@gmail.com>
2025-07-28 15:46:58 +0600

    Update ytb.js

--- src/Commands/Downloader/ytb.js
+++ src/Commands/Downloader/ytb.js
@@ -1,1 +1,20 @@
+Create JavaScript files for the following commands in the folder structure:
 
+src/commands/gen/editpro.js
+src/commands/gen/expend.js
+src/commands/gen/midjourney.js
+
+src/commands/group/spamkick.js
+src/commands/group/supportgc.js
+src/commands/group/topexp.js
+
+src/commands/image/animal.js
+src/commands/image/batslap.js
+src/commands/image/cover.js
+src/commands/image/imgsearch.js
+
+src/commands/info/grouptag.js
+src/commands/info/history.js
+src/commands/info/lyrics.js
+src/commands/info/numinfo.js
+src/commands/info/pokeinfo.js
